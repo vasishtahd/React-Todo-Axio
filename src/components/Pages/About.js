d8e06e9c649bbb102a82c32e86d2c4f99c1b0438
vasishtahd <30791332+vasishtahd@users.noreply.github.com>
2020-04-17 20:39:24 -0700

    React Todo with Axios

---
+++ src/components/Pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About </h1>
+            <p> THis is the Todo List App 1.0.0.
+                This a react crash course.
+            </p>
+
+        </React.Fragment>
+            
+        
+    )
+}
+export default About;
